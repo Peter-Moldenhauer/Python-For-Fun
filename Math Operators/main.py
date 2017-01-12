ebd61c8f2af04cb63e12dbd2e5a88c45ef2af8ce
Peter Moldenhauer <molde003@gmail.com>
2017-01-11 23:29:14 -0600

    Added first python file

---
+++ Math Operators/main.py
@@ -0,0 +1,20 @@
+# Name: Peter Moldenhauer
+# Date: 1/11/17
+# Description: This program demonstrates some basic math operators in Python
+
+# Basic Division - result is 4.5
+print(18 / 4)
+
+# Use 2 forward slashes to round down to the nearest whole number (drops the decimal place)
+# result is 4
+print(18 // 4)
+
+# Use the modulus (%) to access only the remainder - result is 2
+print(18 % 4)
+
+# Calculate 5 to the 3rd power (1st way) - result is 124
+print(5 * 5 * 5)
+
+# Calculate 5 to the 3rd power (2nd way using double astriks **) - result is 125
+# Same thing as 5 to the power of 3, double ** means "calculate the powers" 
+print(5 ** 3)
