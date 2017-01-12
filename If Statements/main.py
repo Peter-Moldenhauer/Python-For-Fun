351e1ecd3777f8ac5e0652dc1b8d667a407673b9
Peter Moldenhauer <molde003@gmail.com>
2017-01-12 15:21:32 -0600

    Added python if statements file

---
+++ If Statements/main.py
@@ -0,0 +1,27 @@
+# Name: Peter Moldenhauer
+# Date: 1/12/17
+# Description: This program demonstrates if statements in Python - if, elif, else
+
+# Example 1:
+age = 12
+
+if age < 21:
+    print("Too young to buy beer!")
+
+
+
+# Example 2:
+name = "Rachel"
+
+if name is "Peter":  # you can use the keword is to compare strings (and also numbers), it means ==
+    print("Hello Peter!")
+elif name is "Rachel":
+    print("Good afternoon Rachel")
+elif name is "Bob":
+    print("Yo Bob what up!")
+else:
+    print("Um I don't know your name!")
+
+
+
+
